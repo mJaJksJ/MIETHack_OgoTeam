c1c871abb57a0f80ef173bfa7f16946ca5b9afee
Dikin01 <kondim007@mail.ru>
2022-05-29 00:04:50 +0300

    hot fix

---
+++ Ogo/Controllers/StudentController/StudentFullRequest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Ogo.Controllers.StudentController
+{
+    public class StudentFullRequest
+    {
+        
+        public int Number { get; set; }
+
+        public string FullName { get; set; }
+        
+        public string GroupName { get; set; }
+       
+        public string BirthDay { get; set; }
+
+        public IFormFile Image { get; set; }       
+        
+        public int NumberOfOrderOfHostel { get; set; }
+     
+        public int NumberOfOrderOfEnrollment { get; set; }
+  
+        public string DateOfEnrollment { get; set; }
+        
+        public string PlaceOfBirth { get; set; }
+
+        public int? NumberOfHousing { get; set; }
+        public int? NumberOfRoom { get; set; }
+
+    }
+}
